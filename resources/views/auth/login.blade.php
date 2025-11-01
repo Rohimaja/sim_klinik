a4a606206bff7e0260400814e7786f89712ada45
edwink29 <edwn.k29@gmail.com>
2025-11-01 22:29:55 +0700

    verif email

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -66,5 +66,13 @@
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+
+        <div class="flex items-center justify-center mt-4">
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md"
+                href="{{ route('verification.form') }}">
+                {{ __('Verifikasi Email?') }}
+            </a>
+        </div>
+
     </form>
 </x-guest-layout>
