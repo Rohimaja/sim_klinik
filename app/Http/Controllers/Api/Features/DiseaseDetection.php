4f57041da5284d1d313f4a1c0d621a658ff91137
AldoR2 <tokoumur@gmail.com>
2025-11-23 01:09:21 +0700

    feat: adding templating disease treatment

--- app/Http/Controllers/Api/Features/DiseaseDetection.php
+++ app/Http/Controllers/Api/Features/DiseaseDetection.php
@@ -15,7 +15,7 @@ class DiseaseDetection extends Controller
                 'image' => 'required|image|max:2048'
             ]
         );
-        $file = $request->file('image'); 
+        $file = $request->file('image');
         $url = env('FLASK_URL');
 
         $response = Http::attach(
@@ -29,14 +29,43 @@ class DiseaseDetection extends Controller
                 'status' => 'error',
                 'message' => 'Flask API error',
                 'flask_error' => $response->json(),
-
             ], 500);
         }
+
+        $classId = $response->json()['class_id'];
+
+        $diseaseTreatment = match ($classId) {
+            0 => [''],
+            1 => [
+                'Cuci area dengan sabun lembut, keringkan benar-benar.',
+                'Gunakan krim antijamur OTC (yang mengandung clotrimazole/ketoconazole/terbinafine) sesuai petunjuk kemasan.',
+                'Hindari menggosok terlalu keras untuk mencegah iritasi.',
+                'Bila muncul di telapak tangan/kaki tapi tidak menimbulkan rasa sakit, tetap aman menunda ke klinik 1-2 hari sambil observasi.',
+                'Pantau perubahan warna: bila warna bertambah gelap, melebar cepat, atau bentuknya tidak simetris'
+            ],
+            2 => [
+                'Gunakan krim antijamur OTC (clotrimazole/terbinafine) pada area bercak merah berbentuk cincin.',
+                'Jaga area tetap kering dan bersih.',
+                'Cuci pakaian, handuk, dan sprei yang bersentuhan dengan lesi untuk mencegah penularan ulang.',
+                'Hindari garukan agar tidak menyebar ke area lain.',
+                'Hindari penggunaan steroid topikal (salep merah/salep kombinasi dengan steroid) karena dapat memperburuk ringworm.'
+            ],
+            3 => [
+                'Gunakan sampo antijamur OTC (mis. ketoconazole/shampoo sulfur selenium) sebagai body wash pada area berbintik',
+                'Jaga kulit tetap kering setelah mandi.',
+                'Hindari keringat berlebihan (mis. olahraga berat) untuk sementara',
+                'Gunakan pakaian longgar dan berbahan katun.',
+            ],
+            default => [''],
+        };
 
         return response()->json([
             'status' => 'success',
             'message' => 'Klasifikasi berhasil dilakukan',
-            'data' => $response->json()
+            'data' => [
+                'prediction' => $response->json(),
+                'treatment' => $diseaseTreatment,
+            ]
         ]);
     }
 }
